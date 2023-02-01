24cf78a26efd9d837127d62fcaeb4ac9c47ae038
isminiz <rsparmaksiz@gmail.com>
2023-02-01 17:30:00 +0300

    2023.2

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,8 +1,37 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Test {
+    public static void main(String[] args) {
     // Satır-1
     // Satır-2
     // Satır-3333t
     // Satır-3333t
     // Satır-3333t
-    // Satır-4444
+    // Gurkay Alanı Başlangıc
+        System.out.println(1);
+        System.out.println(2);
+
+    /*
+    Gurkay Alanı Bitiş
+    Ramazan Alanı Başlangıc
+    */
+
+        System.out.println(3);
+        System.out.println(4);
+
+    /*
+    Ramazan Alanı Bitiş
+    Esra Alanı Başlangıc
+    */
+        System.out.println(5);
+        System.out.println(6);
+
+    /*
+    Esra Alanı Bitiş
+    Zeliha Alanı Başlangıc
+    */
+        System.out.println(7);
+        System.out.println(8);
+
+    }
 }
